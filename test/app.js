dab4de0f2863f5c7317a482d7175072d3c028caa
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2019-03-25 17:59:58 +0100

    further work...

--- test/app.js
+++ test/app.js
@@ -2,14 +2,20 @@
 var assert = require("assert");
 var app = require("../app");
 
-before(function() {
-  console.log("Salamnader");
+var testPost1 = {
+  title: "foo title",
+  content: "bar content",
+  author: "baz author",
+  tags: ['foo-tag', 'bar-tag']
+}
+
+before(function(done) {
+  app.createPost(testPost1);
 })
 
 describe("getAllPosts", function() {
-  context("with no posts created", function() {
     it("returns an empty list", function() {
       assert.equals(app.getAllPosts().length, 0);
     });
-  })
+
 });
