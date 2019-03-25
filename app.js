d9739851f732f6d76efc46d12175fdca6cf474e6
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2019-03-25 01:26:08 +0100

    callbacks example

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-console.log("Hello world")
+function readFromDatabase(callback) {
+  function whenDone() {
+    callback(null, "An amazingly awesome response");
+  }
 
-function tellTime() {
-  console.log("It is now: " + new Date().toISOString())
+  // Pretend to take some time
+  setTimeout(whenDone, 1000);
 }
 
-setTimeout(tellTime, 500);
-setInterval(tellTime, 1000);
+readFromDatabase((error, response) => {
+  if (error) {
+    console.log("We got an error: " + error);
+    return;
+  }
+
+  console.log("Got this response:");
+  console.log(response);
+})
+
