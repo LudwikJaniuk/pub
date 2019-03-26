8fb9fc856d4d0e1aa5eaf70996ab59ae319d15aa
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2019-03-26 19:48:27 +0100

    tape test

--- test/dummy.js
+++ test/dummy.js
@@ -1,27 +1,22 @@
-// Test file for mocha
-var assert = require("assert");
+var test = require("tape");
 var sum = require("../sum").sum;
 
-describe("sum", function() {
-  context("with numbers", function() {
-    var tests = [
-      { a: 1, b: 2, expected: 3 },
-      { a: 4, b: 9, expected: 13 },
-      { a: 0, b: 0, expected: 0 },
-      { a: -1, b: 2, expected: 1 },
-      { a: 0.1, b: 0.2, expected: 0.3 },
-    ]
-    tests.forEach((test) => {
-      it(test.a + " + " + test.b + " = " + test.expected, function() {
-        assert.equal(sum(test.a, test.b), test.expected);
-      });
-    });
-  })
+var tests = [
+  { a: 1, b: 2, expected: 3 },
+  { a: 4, b: 9, expected: 13 },
+  { a: 0, b: 0, expected: 0 },
+  { a: -1, b: 2, expected: 1 },
+  { a: 0.1, b: 0.2, expected: 0.3 },
+]
 
+tests.forEach((spec) => {
+  test(spec.a + " + " + spec.b + " = " + spec.expected, function(assert) {
+    assert.equal(sum(spec.a, spec.b), spec.expected);
+    assert.end();
+  });
+});
 
-  context("with strings", function() {
-    it("should concatenate strings", function() {
-      assert.equal(sum("abc", "def"), "abcdef");
-    });
-  })
+test("strings", (assert) => {
+  assert.equal(sum("abc", "def"), "abcdef");
+  assert.end();
 });
