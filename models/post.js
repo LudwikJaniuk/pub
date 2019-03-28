838ca9d355fec53b0213a9cc3980603a67a90523
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2019-03-28 23:35:02 +0100

    some serious scaffolding

--- models/post.js
+++ models/post.js
@@ -1,9 +1,42 @@
 lodash = require("lodash");
 
-posts = []
+posts = [];
+
+// Generates a new ID every call. 
+// Eploiting closures in Javascript - ask me about this feature!
+nextID = function() { var x = 0; return () => {x++; return x;}}()
 
 module.exports= {
   getAll: function(cb) {
     cb(null, lodash.cloneDeep(posts)); // This will save you many a headache - always clone resources your're returning
+  },
+  deleteAll: function(cb) {
+    posts = [];
+    cb(null);
+  },
+  create: function(postData, cb) {
+    tags = [];
+    if (postData.tags) {
+      for(var tag of postData.tags) {
+        if (typeof(tag) !== "string") {
+          cb("Invalid tag " + tag);
+          return;
+        }
+
+        tags.push(tag);
+      }
+    }
+
+    newPost = {
+      title: postData.title,
+      contents: postData.contents,
+      author: postData.author,
+      tags: tags, 
+      postDate: Date(),
+      id: nextID(),
+    }
+
+    posts.push(newPost);
+    cb(null);
   }
 }
--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@
 var assert = require("assert");
 var request = require("supertest");
 var app = require("../app");
+var Post = require("../models/post");
+var async 
 
 var testPost1 = {
   title: "foo title",
@@ -10,7 +12,14 @@ var testPost1 = {
   tags: ['foo-tag', 'bar-tag']
 }
 
+
+
 describe("getAllPosts", function() {
+  before(function(done) {
+      Post.deleteAll(done);
+  });
+
+  context("With no posts", function () {
     it("Returns a well-formatted empty response.", function(done) {
       request(app)
         .get("/api/")
@@ -18,5 +27,21 @@ describe("getAllPosts", function() {
         .expect({"posts": []})
         .end(done);
     });
+  });
+
+  context("With one post existing", function() {
+    before(function(done) {
+      Post.create(testPost1, done);
+    });
+
+    it("Returns a well-formatted empty response.", function(done) {
+      request(app)
+        .get("/api/")
+        .expect(200)
+        .expect({"posts": [testPost1]})
+        .end(done);
+    });
+  });
 });
+
 
